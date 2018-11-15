7c54f4efa59d31ba4bca6c5f0630c94b47450e81
ellisrichardj <richard.ellis@apha.gsi.gov.uk>
2018-11-15 14:37:40 +0000

    Add further comments on requirements for vanilla install

--- Install_dependancies.sh
+++ Install_dependancies.sh
@@ -3,9 +3,10 @@ set -e
 
 # This script will fetch and install the dependancies required for the BovTB-nf process.
 # The dependancies are generally standard bioinformatics tools
-# There are some standard prerequites for a vanilla linux install such as make, make-guile, gcc, zip, zlib-dev, zlib1g-dev,
-# libncurses5-dev, libbz2-dev, liblzma-dev
-# e.g. on Ubuntu: sudo apt install make make-guile gcc zip zlib-dev zlib1g-dev libncurses5-dev libbz2-dev liblzma-dev
+# There are some standard prerequites for a vanilla linux install such as make, make-guile, gcc, zlib-dev, zlib1g-dev,
+# libncurses5-dev, libbz2-dev, liblzma-dev, python (not python3), python-numpy, python-pip
+# e.g. on Ubuntu: sudo apt install make make-guile gcc zlib-dev zlib1g-dev libncurses5-dev libbz2-dev liblzma-dev python python-numpy, python-pip
+# Followed by pip install biopython
 
 
 # FastUniq
@@ -41,6 +42,16 @@ cd ..
 
 wget https://github.com/lh3/samtools/blob/master/bcftools/vcfutils.pl
 
+# get some test data
+
+#mkdir Data
+#cd Data
+#wget -r -l2 -A ERR84179*.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR841/ 
+#mv ftp.sra.ebi.ac.uk/vol1/fastq/ERR841//*_1.fastq.gz $PWD
+#mv ftp.sra.ebi.ac.uk/vol1/fastq/ERR841//*_2.fastq.gz $PWD
+#rm -r ftp.sra.ebi.ac.uk/
+
+
 # anything else??
 
 
@@ -48,3 +59,6 @@ wget https://github.com/lh3/samtools/blob/master/bcftools/vcfutils.pl
 # Add locations to nextflow.config
 
 echo "params.dependPath = "\"$PWD"\"" >> BovTB-nf/nextflow.config
+
+echo "Dependancies installed sucessfully!"
+
