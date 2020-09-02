4f94ad500eddcb7e4959f021e6635a0111dc7fe3
Aaron Fishman <uk.afishman@gmail.com>
2020-09-02 11:27:47 +0100

    separating out download tests

---
+++ download-tests.sh
@@ -0,0 +1,10 @@
+cd ..
+
+mkdir TestData
+cd TestData
+wget -r -l2 -A ERR84179*.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR841/ 
+mv ftp.sra.ebi.ac.uk/vol1/fastq/ERR841/*/*_1.fastq.gz $PWD
+mv ftp.sra.ebi.ac.uk/vol1/fastq/ERR841/*/*_2.fastq.gz $PWD
+rm -r ftp.sra.ebi.ac.uk/
+
+cd ..
